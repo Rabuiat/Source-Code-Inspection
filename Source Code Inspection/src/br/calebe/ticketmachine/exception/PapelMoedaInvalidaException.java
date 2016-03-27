e239486d768602636ef949efd0cf44a6f3d49b1d
Rafael <Rafael@10.0.1.92>
2016-03-27 15:45:37 -0300

    Correções em código voltadas para classes de execções e vetor percorrido de forma incorreta.

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -5,5 +5,8 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoedaInvalidaException extends Exception {
-    
+
+    public PapelMoedaInvalidaException() {
+        System.out.println("O papel moeda inserido é inválido");
+    }  
 }
